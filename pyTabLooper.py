30b5e403a10ec300ffb6b5b399761c9963d203e4
PEE Sistemas Orlando Daniel Cuevas <pee.ti3@cables.xignux.com>
2017-07-13 17:46:37 -0500

    update code

--- pyTabLooper.py
+++ pyTabLooper.py
@@ -26,42 +26,23 @@ for k, v in dictURL.items():
 
 for contador in range(len(dictLinks)):
     urlOpen = dictLinks[contador]
+    time.sleep(.05)
     webbrowser.open(urlOpen)
 
-time.sleep(1)
+time.sleep(.3)
 kInput.press_key(kInput.control_key)
 kInput.tap_key('1')
 kInput.release_key(kInput.control_key)
-time.sleep(1)
+time.sleep(.3)
 kInput.press_key(kInput.control_key)
 kInput.tap_key('w')
 kInput.release_key(kInput.control_key)
-time.sleep(1)
+time.sleep(.1)
 
-for i in range(len(listNum)):
-    tiempo = listNum[i] * 60
-    time.sleep(tiempo)
-    kInput.press_key(kInput.control_key)
-    print('si pasa')
-    kInput.tap_key(kInput.tab_key)
-    kInput.release_key(kInput.control_key)
-
-
-#####################################
-# Code Dump
-#####################################
-
-	# webbrowser.open("www.yahoo.com")
-	# time.sleep(1)
-	# kInput.press_key(kInput.control_key)
-	# kInput.tap_key(kInput.tab_key)
-	# kInput.release_key(kInput.control_key)
-	# time.sleep(.1)
-	# kInput.press_key(kInput.control_key)
-	# kInput.tap_key('w')
-	# kInput.release_key(kInput.control_key)
-	# time.sleep(5)
-
-# for key, value in d.items():
-
-# {'Links': {'www.google.com': '5', 'www.reddit.com': '5', 'www.yahoo.com': '5', 'www.youtube.com': '5'}}
+while validador == True:
+    for i in range(len(listNum)):
+        tiempo = listNum[i] * 60
+        time.sleep(tiempo)
+        kInput.press_key(kInput.control_key)
+        kInput.tap_key(kInput.tab_key)
+        kInput.release_key(kInput.control_key)
